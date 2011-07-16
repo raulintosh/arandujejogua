70910a5daff8c7567e2a131dba29f7062a64e388
Raul Giucich <raul.giucich@gmail.com>
2011-07-15 22:06:29 -0400

    tutorial

--- src/org/juru/arandu/ArandujejoguaServlet.java
+++ src/org/juru/arandu/ArandujejoguaServlet.java
@@ -1,13 +1,43 @@
 package org.juru.arandu;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.SimpleTimeZone;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 @SuppressWarnings("serial")
 public class ArandujejoguaServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+
+		SimpleDateFormat fmt = new SimpleDateFormat(
+				"yyyy-MM-dd hh:mm:ss.SSSSSS");
+		fmt.setTimeZone(new SimpleTimeZone(0, ""));
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		String navBar;
+		if (user != null) {
+			navBar = "<p>Welcome, " + user.getNickname()
+					+ "! You can <a href=\"" + userService.createLogoutURL("/")
+					+ "\">sign out</a>.</p>";
+		} else {
+			navBar = "<p>Welcome! <a href=\"" + userService.createLoginURL("/")
+					+ "\">Sign in or register</a> to customize.</p>";
+		}
+
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println(navBar);
+		out.println("<p>The time is: " + fmt.format(new Date()) + "</p>");
+
 	}
 }
